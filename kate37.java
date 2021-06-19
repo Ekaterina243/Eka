c833cdd562268c4a2f45b0fbf9f4ea5a23442558
Ekaterina243 <ketreda@gmail.com>
2021-06-19 18:46:55 +0300

    Add kate37java

---
+++ kate37.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		int a;
+		int b;
+	    a=15;
+	    b=3;
+	    n=a-b;
+	    if(n % 2 == 0)
+	   {System.out.println("четное");}
+	   if(n % 2 !=0)
+	  {System.out.println("нечетное");}
+	}
+}
