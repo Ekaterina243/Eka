1edf1eafd3c6a8d08aa6925bd75401c5bef8949e
Ekaterina243 <ketreda@gmail.com>
2021-06-19 19:49:02 +0300

    kate2.java

---
+++ kate2.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			double m = 8.5d;
+      double n = 11.45d;
+      double a = 10d;
+      double dis1 = a - m;
+      double dis2 = a - n;
+      if (dis1 > dis2) {
+        System.out.println(n);
+      } else {
+        System.out.println(m);}
+	   
+	}
+}
